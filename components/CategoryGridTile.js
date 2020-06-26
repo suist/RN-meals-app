d3586f09aa15293ff3a2ee564ac7ca50b0de1072
su <skoco73@gmail.com>
2020-06-26 12:26:05 +0900

    Bottom tab navigator with icon

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -35,6 +35,12 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 15,
     height: 150,
+    borderRadius: 10,
+    overflow:
+      Platform.OS === "android" && Platform.Version >= 21
+        ? "hidden"
+        : "visible",
+    elevation: 5,
   },
   container: {
     flex: 1,
@@ -43,7 +49,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, heigth: 2 },
     shadowRadius: 10,
-    elevation: 3,
+
     padding: 15,
     justifyContent: "flex-end",
     alignItems: "flex-end",
--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,16 @@
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import {
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator,
+} from "react-navigation";
 import { Platform } from "react-native";
-
+import { Ionicons } from "@expo/vector-icons";
 import Colors from "../constants/Colors";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import FavoritesScreen from "../screens/FavoritesScreen";
+import React from "react";
 
 const MealsNavigator = createStackNavigator(
   {
@@ -32,4 +38,38 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+              name="ios-restaurant"
+              size={25}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+      },
+    },
+    Favorites: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
+          );
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+);
+
+export default createAppContainer(MealsFavTabNavigator);
